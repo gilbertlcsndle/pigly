ef995425e67ce3db17c831e575433afb551ab3e9
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-11-29 12:40:31 +0800

    Show error messages

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,8 @@
 class Url < ApplicationRecord
   VALID_URL_REGEX = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix
-  validates :original_url, format: { with: VALID_URL_REGEX }, presence: true
+  validates :original_url,
+    format: { with: VALID_URL_REGEX, allow_blank: true, message: 'Please enter a valid url.' },
+    presence: { message: 'Please enter a url' }
   before_create :generate_short_url
 
   private
